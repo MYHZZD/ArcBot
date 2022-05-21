c816ca49aad27ec6965a827f0f51b737853ac6ab
MYHZZD <brunestudarcueid@gmail.com>
2022-05-21 11:08:38 +0800

    创建名言插件

---
+++ plugins/saying/__init__.py
@@ -0,0 +1,89 @@
+import requests
+import time
+import json
+import os
+import httpx
+import aiofiles
+from typing import Optional
+from pathlib import Path
+from nonebot import get_driver, on_message, on_command
+from nonebot.log import logger
+from nonebot.params import EventMessage, EventPlainText, CommandArg
+from nonebot.adapters import Message
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, Message
+
+from .config import Config
+global_config = get_driver().config
+config = Config.parse_obj(global_config)
+
+jsonpath = "data/saying/"
+jsonname = "data/saying/mappings.json"
+checkexdir = os.path.exists(jsonpath)
+if checkexdir == False:
+   os.makedirs(jsonpath)
+   startdata={'Plugin':'Created by MYHZZD'}
+   with open(jsonname, "w", encoding='utf8') as writejson:
+        json.dump(startdata, writejson, ensure_ascii=False)
+
+
+def download_img(img_url, gid, img_name):
+    filepath = "data/saying/"+gid
+    checkexdir = os.path.exists(filepath)
+    if checkexdir == False:
+        os.makedirs(filepath)
+    filename = "data/saying/"+gid+"/"+img_name+".png"
+    header = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53",
+    }
+    r = requests.get(img_url, headers=header, stream=True)
+    if r.status_code == 200:
+        open(filename, 'wb').write(r.content)
+    del r
+
+def mapping_table(gid,uid, img_name,who_said):
+    unixtime = str(int(round(time.time() * 1000)))
+    data_1={'Added By':uid,'Group':gid,'Picture ID':img_name}
+    data_2={'Who':who_said,'Data':data_1}
+    data_3={unixtime:data_2}
+    with open(jsonname, "r", encoding='utf8') as readfile:
+        jsondata: str = readfile.read()
+        mappings = json.loads(jsondata)
+    mappings.update(data_3)
+    with open(jsonname, "w", encoding='utf8') as writejson:
+        json.dump(mappings, writejson, ensure_ascii=False)
+
+
+matcher = on_command("名言")
+
+
+@matcher.handle()
+async def _(event: GroupMessageEvent, Mes: Message = CommandArg()):
+    gid = str(event.group_id)
+    uid = str(event.user_id)
+    
+    Arg_str: str = str(Mes)
+    if Arg_str == '':
+        return
+    Arg_list = Arg_str.split()
+    who_said = Arg_list[0]
+    if who_said.replace('.\\', ' ') != who_said or who_said.replace('./', ' ') != who_said:
+        await matcher.send('输入非法字符!参数不能带有 / \ . 等符号')
+        return
+
+    for arg in Mes:
+        if arg.type == 'image':
+            img_url: str = arg.data['url']
+            img_name: str = arg.data['file']
+            download_img(img_url, gid, img_name)
+            mapping_table(gid,uid, img_name,who_said)
+
+    await matcher.send(img_name)
+
+
+matcher = on_message(priority=99)
+
+
+@matcher.handle()
+async def _(event: GroupMessageEvent, foo: str = EventPlainText()):
+    return
