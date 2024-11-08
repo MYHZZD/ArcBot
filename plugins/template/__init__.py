75bb465ab1b574a4a0c4c0fef4793048a8aebab8
Arcueid <brunestudarcueid230@gmail.com>
2024-11-09 03:54:49 +0800

    合并转发

--- plugins/template/__init__.py
+++ plugins/template/__init__.py
@@ -1,10 +1,17 @@
-from nonebot import require, get_bot, get_driver, on_command
+from nonebot import require, get_bot, on_command, get_driver
 from nonebot.params import CommandArg
-from nonebot.adapters.onebot.v11 import GroupMessageEvent, Message, MessageSegment
+from nonebot.adapters.onebot.v11 import (
+    GroupMessageEvent,
+    Message,
+    MessageSegment,
+    Event,
+)
 
 
 scheduler = require("nonebot_plugin_apscheduler").scheduler
-GID = 584017557
+
+GID = get_driver().config.testgid
+UID = get_driver().config.testuid
 
 
 async def ck1():  # 固定时间触发
@@ -36,7 +43,7 @@ ck4 = on_command("ck4", priority=90, block=False)
 @ck4.handle()  # finish中止传递
 async def ck3_function(args: Message = CommandArg()):
     if p_args := args.extract_plain_text():
-        await get_bot().send_group_msg(group_id=gid, message="CK4!")
+        await get_bot().send_group_msg(group_id=GID, message="CK4!")
     else:
         await ck4.finish("ck4结束")
     await get_bot().send_group_msg(group_id=GID, message=" " + p_args)
@@ -105,3 +112,63 @@ async def ck7_function(event: GroupMessageEvent):
         ]
     )
     await get_bot().send_group_msg(group_id=GID, message=message)
+
+
+ck8 = on_command("ck8", priority=90, block=True)
+
+
+@ck8.handle()  # classmethod
+async def ck8_function(event: GroupMessageEvent, event2: Event):
+    uid = event.user_id
+    message = Message(
+        [
+            MessageSegment.text("UID=" + str(uid) + " or " + event2.get_user_id()),
+        ]
+    )
+    await get_bot().send_private_msg(user_id=UID, message=message)
+
+
+ck9 = on_command("ck9", priority=90, block=True)
+
+
+@ck9.handle()  # 合并转发
+async def ck9_function(event: GroupMessageEvent):
+
+    uid = event.user_id
+    mid = event.message_id
+    message1 = Message(
+        [
+            # MessageSegment.node(mid),
+            MessageSegment.node_custom(2591573409, "SuperUser", "手动构建消息方法1-1"),
+            MessageSegment.node_custom(2591573409, "SuperUser", "手动构建消息方法1-2"),
+        ]
+    )
+
+    await get_bot().send_group_forward_msg(
+        group_id=GID, messages=message1
+    )  # 注意messages
+
+    message2 = [
+        {
+            "type": "node",
+            "data": {
+                "name": "SuperUser",
+                "uin": 2591573409,
+                "content": "手动构建消息方法2-1",
+            },
+        },
+        {
+            "type": "node",
+            "data": {
+                "name": "SuperUser",
+                "uin": 2591573409,
+                "content": "手动构建消息方法2-2",
+            },
+        },
+    ]
+
+    await get_bot().call_api(
+        "send_group_forward_msg",
+        group_id=GID,
+        messages=message2,
+    )
